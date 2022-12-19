44ea8688a290f810be75c66c242901861c886739
Himanshu Joshi <hsjoshi@iu.edu>
2022-12-19 16:51:25 -0500

    Update TwiloService.java

--- Prosper/src/main/java/com/Prosper/service/TwiloService.java
+++ Prosper/src/main/java/com/Prosper/service/TwiloService.java
@@ -20,8 +20,8 @@ import java.time.LocalDateTime;
 @Service
 public class TwiloService {
 	
-	private  final String ACCOUNT_SID = "AC71e7dd882abb7032ccaf238e5286be48";
-    public  final String AUTH_TOKEN = "97bf4932e155648fdb2b8b16cd1de9b1";
+	private  final String ACCOUNT_SID = <Account_SID>;
+    public  final String AUTH_TOKEN = <Account_Token>;
     
     private final String  TOPIC_DESTINATION = "/topic/sms";
     
